af05472caa248c5af1a0bd8add382ebbd8c775ef
Baek100-KR <100rudtjs@naver.com>
2024-11-13 15:58:16 +0900

    백승민 커밋

---
+++ amr_gui/setup.py
@@ -0,0 +1,31 @@
+import os
+from setuptools import setup, find_packages
+
+package_name = 'amr_gui'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        # ROS 2 패키지 리소스
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        # 이미지 파일 포함
+        (os.path.join('share', package_name, 'images'), 
+            [os.path.join('images', 'eye-6662_256.gif')]),
+    ],
+    install_requires=['setuptools', 'rclpy', 'PyQt5'],
+    zip_safe=True,
+    maintainer='your_name',  # 자신의 이름으로 변경
+    maintainer_email='your_email@example.com',  # 자신의 이메일로 변경
+    description='AMR 식당 로봇 GUI',
+    license='Apache License 2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'amr_gui_node = amr_gui.amr_gui_node:main',
+        ],
+    },
+)
