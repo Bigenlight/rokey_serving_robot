cc614f6b90a802062401155bf161eea1aeb41989
Baek100-KR <100rudtjs@naver.com>
2024-11-14 16:27:02 +0900

    백승민 커밋

--- build/kitchen_gui/colcon_command_prefix_setup_py.sh
+++
@@ -1,2 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/rokey/2_ws/src/install/custom_interface/share/custom_interface/package.sh"
--- install/custom_interface/local/lib/python3.10/dist-packages/custom_interface/srv/__init__.py
+++
@@ -1,1 +0,0 @@
-from custom_interface.srv._order import Order  # noqa: F401
